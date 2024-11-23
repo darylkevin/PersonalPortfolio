e952bd043790f1fdfb57606046a3828f7ff1bb4e
Daryl Kevin <piang@Daryls-MacBook-Pro.local>
2024-11-24 02:43:40 +0800

    added navs for sections

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ import Experiences from "./ui/Experiences";
 import Footer from "./ui/Footer";
 import Hero from "./ui/Hero";
 import Projects from "./ui/Projects";
+import { M_PLUS_1 } from "next/font/google";
 
 // mobile-phones: 320px-480px
 // tablets: 768px-1024px
@@ -20,33 +21,41 @@ import Projects from "./ui/Projects";
 
 export default function Home() {
 
-  const [scrollStatus, setScrollStatus] = useState('start')
-  const [experiencesSectionBoundingElement, setExperiencesSectionBoundingElement] = useState(null);
-  const [projectsSectionBoundingElement, setProjectsSectionBoundingElement] = useState(null);
-  const [educationSectionBoundingElement, setEducationSectionBoundingElement] = useState(null);
+  const [scrollStatus, setScrollStatus] = useState('experiences')
 
   useEffect(() => {
-    const experiences = document.getElementById('experiences')
-    const projects = document.getElementById('projects')
-    const education = document.getElementById('education')
+    const updateScrollStatus = () => {
+      const currentScrollHeight = window.scrollY + window.innerHeight
+      const experiencesTop = document.getElementById('experiences')?.offsetTop
+      const projectsTop = document.getElementById('projects')?.offsetTop
+      const educationTop = document.getElementById('education')?.offsetTop
+      
+      if (currentScrollHeight < 1.5 * projectsTop) {
+        setScrollStatus('experiences')
+      } else if (currentScrollHeight < educationTop) {
+        setScrollStatus('projects')
+      } else {
+        setScrollStatus('education')
+      }
+    }
 
-    console.log(experiences?.getBoundingClientRect())
-    console.log(projects?.getBoundingClientRect())
-    console.log(education?.getBoundingClientRect())
+    window.addEventListener('scroll', updateScrollStatus)
+
+    return () => {
+      window.removeEventListener('scroll', updateScrollStatus)
+    }
   }, [])
-
-
 
   return (
     <div className="mx-auto max-w-screen-xl">
       <div className="mx-auto grid grid-cols-1 px-8 xs:max-w-sm sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] lg:grid-cols-2 lg:px-12 xl:max-w-[1280px]">
         <header className="py-16 lg:sticky lg:top-0 lg:min-h-[640px] lg:max-h-screen lg:py-24">
-          <Hero />
+          <Hero scrollStatus={scrollStatus}/>
         </header>
         <main className="py-16 lg:py-24">
-          <Experiences className="section"/>
-          <Projects className="section"/>
-          <Education className="section"/>
+          <Experiences />
+          <Projects />
+          <Education />
           <Footer />
         </main>
       </div>
--- app/ui/Hero.tsx
+++ app/ui/Hero.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { hero, profileLinks, profileLinkKey } from "../lib/definitions";
 
-const Hero = () => {
+const Hero = ({ scrollStatus }) => {
   return (
     <section className="flex border-[1px] border-red-400 pt-2 font-bold place-items-center h-full text-center">
       <div className="flex-col justify-between">
@@ -13,6 +13,38 @@ const Hero = () => {
             {hero.title}
           </p>
           <p className="text-md py-4 font-extralight lg:py-8">{hero.about}</p>
+
+          <div className="flex gap-8 py-4 pb-8 h-24 justify-center max-lg:hidden">
+            <div className="relative hover:cursor-pointer" onClick={() => window.scrollTo(scrollX, document.getElementById('experiences')?.offsetTop)}>
+              <span className={`${scrollStatus === 'experiences' ? 'text-lg' : 'text-sm opacity-50'} blur-2xl font-bold select-none transition-all`}>
+              Experiences
+              </span>
+              <h1
+                  className={`${scrollStatus === 'experiences' ? 'text-lg' : 'text-sm opacity-50 pt-1'} absolute inset-0 font-bold select-auto transition-all`}>
+                  Experiences
+              </h1>
+            </div>
+            <div className="relative hover:cursor-pointer" onClick={() => window.scrollTo(scrollX, document.getElementById('projects')?.offsetTop)}>
+              <span className={`${scrollStatus === 'projects' ? 'text-lg' : 'text-sm opacity-50 pt-1'} blur-2xl font-bold select-none transition-all`}>
+              Projects
+              </span>
+              <h1
+                  className={`${scrollStatus === 'projects' ? 'text-lg' : 'text-sm opacity-50 pt-1'} absolute inset-0 font-bold select-auto transition-all`}>
+                  Projects
+              </h1>
+            </div>
+            <div className="relative hover:cursor-pointer" onClick={() => window.scrollTo(scrollX, document.getElementById('education')?.offsetTop)}>
+              <span className={`${scrollStatus === 'education' ? 'text-lg' : 'text-sm opacity-50 pt-1'} blur-2xl font-bold select-none transition-all`}>
+              Education
+              </span>
+              <h1
+                  className={`${scrollStatus === 'education' ? 'text-lg' : 'text-sm opacity-50 pt-1'} absolute inset-0 font-bold select-auto transition-all`}>
+                  Education
+              </h1>
+            </div>
+          </div>
+
+
           <div className="flex gap-2 justify-center">
             {Object.keys(profileLinks).map((link) => {
               const profileLink = profileLinks[link as profileLinkKey];
